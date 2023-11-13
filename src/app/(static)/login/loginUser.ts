a725a83dcdfc74b9ce3dafb9d92f21e003d0b0e1
mahmoudbahaa755 <mahmoudbaha755@gmail.com>
2023-11-13 20:52:36 +0200

    uploading

--- src/app/(static)/login/loginUser.ts
+++ src/app/(static)/login/loginUser.ts
@@ -1,36 +1,29 @@
 'use client';
-import axios from 'axios';
+
 import { useRouter } from 'next/router';
 
-export default function LoginAPI(email: string, password: string) {
-  const router = useRouter();
-
-  const loginUser = async (email: string, password: string) => {
-    try {
-      const response = await axios.post('https://endo-api.eductor.org/auth/login/dashboard', {
+export default async function LoginAPI(email: string, password: string) {
+  try {
+    const response = await fetch('https://endo-api.eductor.org/auth/login/dashboard', {
+      method: 'POST',
+     
+      body: JSON.stringify({
         email: email,
         password: password,
-      });
+      }),
+    });
 
-      if (response.status === 200) {
-        const userData = response.data.data; 
-
-        console.log(userData);
-
-        // Redirect to home page
-        router.push('./');
-      } else {
-        console.error('Login failed:', response.data.message);
-        // Show error message
-        alert('Login failed: ' + response.data.message);
-      }
-    } catch (error) {
-      console.error('Login error:', error);
-      // Show error message
-      alert('Login error: ' + error);
+    if (response.status === 200) {
+      const userData = await response.json();
+      console.log(userData);
+      return userData;
+    } else {
+      const errorData = await response.json();
+      console.error('Login failed:', errorData.message);
+      return null;
     }
-  };
-  const x=loginUser(email,password)
-  return x;
-  // ...
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
