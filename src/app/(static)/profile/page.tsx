067a37d66b3c22d8de0dfb7bf21ceadcea452d5a
mahmoudbahaa755 <mahmoudbaha755@gmail.com>
2023-11-14 03:35:22 +0200

    .

--- src/app/(static)/profile/page.tsx
+++ src/app/(static)/profile/page.tsx
@@ -4,8 +4,11 @@ import Messages from "./Messages";
 import EditProfile from "./EditProfile";
 import { useState } from "react";
 import UserProfile from "./userProfile";
+import Button from '@/elements/Button'
 export default function ProfilePage() {
-    const [section,setSection]=useState('profile')
+    const [section, setSection] = useState('profile')
+    const sections = ['profile', 'edit'];
+
     return (
         <div className="container mt-3 px-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
@@ -13,10 +16,12 @@ export default function ProfilePage() {
                     <Profile />
                 </div>
                 <div className=' bg-inner'>
-                    <div className="flex   flex-col sm:flex-row justify-space gap-3">
-                        <div onClick={() => setSection('profile')}>profile</div>
-                        {/* <div onClick={() => setSection('messages')}>messages</div> */}
-                        <div onClick={() => setSection('edit')}>edit</div>
+                    <div className="flex   flex-col sm:flex-row justify-around gap-3">
+                        {sections.map((section) => (
+                            <div key={section} onClick={() => setSection(section)}   >
+                                <Button type='button' name={section}/>
+                            </div>
+                        ))}
                     </div>
                     <div className="mt-8">
                         {section === 'profile' && <UserProfile />}
--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ type, name, classes }) => {
     return (
-        <button type={type} className={classes}>{name}</button>
+        <button type={type} className={classes ||"mt-2 cursor-pointer text-center py-2 px-4 rounded-lg bg-blue-500 text-white hover:bg-blue-700 transition-colors duration-200"}>{name}</button>
     );
 }
 
